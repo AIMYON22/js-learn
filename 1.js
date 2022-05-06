f595583b31c08d94362e7f99ce82026951e761f2
Meseta <1608741852@qq.com>
2022-05-06 16:35:35 +0800

    create user

---
+++ 1.js
@@ -0,0 +1,14 @@
+let meseta = {
+    name: 'meseta',
+    sex: 'female',
+    height: '175cm',
+    weight: '65kg',
+    age: '32'
+}
+let mingxin = {
+    name: 'lu mingxin',
+    sex: 'female',
+    height: '170cm',
+    weight: '50kg',
+    age: '22'
+}
