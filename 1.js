0b8f5a7bb560dccbd391fa61e4a962685c34aa9c
Meseta <1608741852@qq.com>
2022-05-06 17:41:57 +0800

    modify 1.js

--- 1.js
+++ 1.js
@@ -9,6 +9,26 @@ let mingxin = {
     name: 'lu mingxin',
     sex: 'female',
     height: '170cm',
-    weight: '50kg',
+    weight: '52kg',
     age: '22'
 }
+meseta = new Proxy(meseta, {
+    get(target, prop) {
+        if(prop in target) {
+            return target[prop];
+        }
+        else {
+            return 'cant find this porp'
+        }
+    },
+
+    set(target, prop, value) {
+        target[prop] = value;
+        return true;
+    }
+})
+
+alert(meseta.name);
+alert(meseta.haha);
+meseta.name = 'aimyon'
+alert(meseta.name)
